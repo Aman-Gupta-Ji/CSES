9d9a1c4e4895001748b7cc198c36da2aa1be0fff
Aman Gupta <amanguptaprogrammer@gmail.com>
2022-09-21 21:58:18 +0530

    Monsters

--- Monsters(1).cses.cpp
+++ Monsters(1).cses.cpp
@@ -1,110 +1,97 @@
 //link : https://cses.fi/problemset/task/1194/
-//status: ACCEPTED
+//status: TLE
 
 #include<bits/stdc++.h>
-
-#define pii pair<int, int>
-#define mn 1005
 using namespace std;
 
-int N, M;
-queue<pii> q;
-int paths[mn][mn];
-pii from[mn][mn];
-int oo = INT_MAX;
-pii A;
-string ans;
-bool possible = false;
+int n, m;
+bool visited[1001][1001];
+ 
+string dirC = "RDLU";
+int dir[] = {0, 1, 0, -1, 0};
+ 
+pair<int, string> dfs(vector<string>& grid, int i, int j, int len) {
+    // 0 means success
+    if(i < 0 || i >= n || j < 0 || j >= m)
+        return make_pair(-1, "O");
+ 
+    if(grid[i][j] == '#' || visited[i][j])
+        return make_pair(-1, "O"); // -ve for wall
+ 
+    if(grid[i][j] == 'M')
+        return make_pair(1, "O"); // +ve distance for Moster
+ 
+    len++;
+    int d = -1;
+    string res = "O";
+ 
+    int ni, nj;
+    char s;
+ 
+    pair<int, string> go;
+ 
+    visited[i][j] = true;
+    bool can = true;
+ 
+    for(int k = 0; k < 4; k++) {
+        ni = i + dir[k];
+        nj = j + dir[k + 1];
+        s = dirC[k];
+        go = dfs(grid, ni, nj, len);
+        if(go.first < 0)
+            continue;
+        if(go.first == 0) {
+            if(can && (d != 0 || res.length() > go.second.length() + 1)) {
+                d = 0;
+                res = s + go.second;
+            }
+        }
+        else if(go.first < len) {
+            if(d <= 0)
+                d = go.first + 1;
+            else
+                d = min(d, go.first + 1);
+            // cout<<len<<" -> "<<i<<" "<<j<<" : "<<d<<endl;
+            res = "O";
+            can = false;
+        }
+    }
+ 
+    visited[i][j] = false;
 
-void retrace(pii node)
-{
-    pii origin = from[node.first][node.second];
-    if (origin == pii(0, 0))
-        return;
-    if (origin.first == node.first + 1)
-        ans.push_back('U');
-    if (origin.first == node.first - 1)
-        ans.push_back('D');
-    if (origin.second == node.second + 1)
-        ans.push_back('L');
-    if (origin.second == node.second - 1)
-        ans.push_back('R');
-    retrace(origin);
+    if(d <= 0 && (i == 0 || j == 0 || i == n - 1 || j == m - 1))
+        return make_pair(0, ""); // reached the destination
+
+    return make_pair(d, res);
 }
-void check(pii origin, pii dest)
-{
-    int pl = paths[origin.first][origin.second];
-    if (pl + 1 < paths[dest.first][dest.second])
-    {
-        paths[dest.first][dest.second] = pl + 1;
-        q.push(dest);
-        from[dest.first][dest.second] = origin;
-    }
+ 
+pair<int, string> getPath(vector<string>& grid) {
+    int i, j;
+    memset(visited, false, sizeof(visited));
+ 
+    for(i = 0; i < n; i++)
+        for(j = 0; j < m; j++)
+            if(grid[i][j] == 'A')
+                return dfs(grid, i, j, 0);
+ 
+    return make_pair(1, "");
 }
-bool mora = false;
-void bfs()
-{
-    while (!q.empty())
-    {
-        pii loc = q.front(), next;
-        q.pop();
-        next = loc;
-        next.first++;
-        check(loc, next);
-        next = loc;
-        next.first--;
-        check(loc, next);
-        next = loc;
-        next.second++;
-        check(loc, next);
-        next = loc;
-        next.second--;
-        check(loc, next);
-        if (mora && (loc.first == 1 || loc.second == 1 || loc.first == N || loc.second == M))
-        {
-            cout << "YES" << endl;
-            cout << paths[loc.first][loc.second] << endl;
-            retrace(loc);
-            possible = true;
-            return;
-        }
-    }
-}
-int main()
-{
-    cin >> N >> M;
-    for (int i = 1; i <= N; i++)
-    {
-        string s;
-        cin >> s;
-        for (int j = 1; j <= M; j++)
-        {
-            paths[i][j] = 1e9;
-            if (s[j - 1] == '#')
-                paths[i][j] = 0;
-            if (s[j - 1] == 'M')
-            {
-                q.push(pii(i, j));
-                paths[i][j] = 0;
-            }
-            if (s[j - 1] == 'A')
-            {
-                A.first = i;
-                A.second = j;
-            }
-        }
-    }
-    bfs();
-    mora = true;
-    from[A.first][A.second] = pii(0, 0);
-    paths[A.first][A.second] = 0;
-    q.push(A);
-    bfs();
-    if (possible)
-    {
-        reverse(ans.begin(), ans.end());
-        cout << ans << endl;
+ 
+int main(void) {
+    cin>>n>>m;
+    vector<string> grid(n);
+    for(int i = 0; i < n; i++)
+        cin>>grid[i];
+ 
+    pair<int, string> res = getPath(grid);
+ 
+    if(!res.first) {
+        cout<<"YES"<<endl;
+        cout<<res.second.length()<<endl;
+        cout<<res.second<<endl;
     }
     else
-        cout << "NO" << endl;
+        cout<<"NO"<<endl;
+ 
+    return 0;
 }
